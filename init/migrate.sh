4f5e98a89c355bd47630a9c1ab8ea5bbdde51037
Steve Eardley <steve@eardley.xyz>
2021-05-24 18:03:24 +0100

    generalise the script to take arbitrary new and old URLs rather than just localhost

--- init/migrate.sh
+++ init/migrate.sh
@@ -5,15 +5,19 @@
 ##############
 #
 # Description: 
-# Updates URL in database so the site can run on localhost
-#
+# Updates URL in database from OLD_URL to NEW_URL
 
-export dev_url=http://localhost:8000
-
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -D$MYSQL_DATABASE -e "
-UPDATE ${WORDPRESS_TABLE_PREFIX}options SET option_value = REPLACE(option_value, '${PRODUCTION_URL}', '${dev_url}') WHERE option_name = 'home' OR option_name = 'siteurl'; 
-UPDATE ${WORDPRESS_TABLE_PREFIX}posts SET guid = REPLACE(guid, '${PRODUCTION_URL}', '${dev_url}'); 
-UPDATE ${WORDPRESS_TABLE_PREFIX}posts SET post_content = REPLACE(post_content, '${PRODUCTION_URL}', '${dev_url}'); 
-UPDATE ${WORDPRESS_TABLE_PREFIX}posts SET post_content = REPLACE(post_content, 'src=\"${PRODUCTION_URL}\"', 'src=\"${dev_url}\"'); 
-UPDATE ${WORDPRESS_TABLE_PREFIX}posts SET guid = REPLACE(guid, '${PRODUCTION_URL}', '${dev_url}') WHERE post_type = 'attachment'; 
-UPDATE ${WORDPRESS_TABLE_PREFIX}postmeta SET meta_value = REPLACE(meta_value, '${PRODUCTION_URL}', '${dev_url}');"
+if [ -z "$NEW_URL" ]
+then
+   echo "No new URL specified, keeping ${OLD_URL}"
+else
+  echo "Migrating wordpress DB from ${OLD_URL} to ${NEW_URL}..."
+  mysql -uroot -p$MYSQL_ROOT_PASSWORD -D$MYSQL_DATABASE -e "
+  UPDATE ${WORDPRESS_TABLE_PREFIX}options SET option_value = REPLACE(option_value, '${OLD_URL}', '${NEW_URL}') WHERE option_name = 'home' OR option_name = 'siteurl'; 
+  UPDATE ${WORDPRESS_TABLE_PREFIX}posts SET guid = REPLACE(guid, '${OLD_URL}', '${NEW_URL}'); 
+  UPDATE ${WORDPRESS_TABLE_PREFIX}posts SET post_content = REPLACE(post_content, '${OLD_URL}', '${NEW_URL}'); 
+  UPDATE ${WORDPRESS_TABLE_PREFIX}posts SET post_content = REPLACE(post_content, 'src=\"${OLD_URL}\"', 'src=\"${NEW_URL}\"'); 
+  UPDATE ${WORDPRESS_TABLE_PREFIX}posts SET guid = REPLACE(guid, '${OLD_URL}', '${NEW_URL}') WHERE post_type = 'attachment'; 
+  UPDATE ${WORDPRESS_TABLE_PREFIX}postmeta SET meta_value = REPLACE(meta_value, '${OLD_URL}', '${NEW_URL}');"
+  echo "done!"
+fi
